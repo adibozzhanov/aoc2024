fb14da75c5a1ef3dde1740b891fe8951a7b725dc
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-12-06 23:54:41 +0000

    day6

--- helpers.nix
+++ helpers.nix
@@ -2,10 +2,26 @@ with (import <nixpkgs>{}).lib.strings;
 with (import <nixpkgs>{}).lib.lists;
 with builtins;
 rec {
-  sum = l: foldl (acc: e: acc+e) 0 l;
-  abs = x: if (x < 0) then -x else x;
-  pairs = l: init (imap0 (i: v: take 2 (drop i l)) l);
+  # vector operations
+  vecStr = v: "${toString v.x},${toString v.y}";
+  sameDirection = a: b: (dotVec a b) > 0;
+  magSquared = a: dotVec a a;
+  collinear = a: b: (a.x * b.y - a.y * b.x) == 0;
+  scaleVec = i: v: {x = v.x * i; y = v.y * i;};
+  dotVec = a: b: (a.x * b.x + a.y * b.y);
+  addVec = a: b: {x = (a.x + b.x); y = (a.y + b.y);};
+  subVec = a: b: {x = (a.x - b.x); y = (a.y - b.y);};
+
+  # string processing
+  explode = s: init (tail (splitString "" s));
   lines = s: init (splitString "\n" s);
-  stringGrid = s: (map (x: filter (y: y != "") (splitString "" x)) (lines s));
+  stringGrid = s: (map (x: filter (y: y != "") (explode x)) (lines s));
   spacedInts = s: map toInt (splitString " " s);
+
+  # lists
+  sum = l: foldl (acc: e: acc+e) 0 l;
+  pairs = l: init (imap0 (i: v: take 2 (drop i l)) l);
+
+  # numbers
+  abs = x: if (x < 0) then -x else x;
 }
--- template.nix
+++ template.nix
@@ -1,7 +1,9 @@
 with builtins;
+with import ../helpers.nix;
 with (import <nixpkgs> { }).lib.lists;
 with (import <nixpkgs> { }).lib.strings;
-with import ../helpers.nix;
+with (import <nixpkgs> { }).lib.debug;
+with (import <nixpkgs> { }).lib.trivial;
 let
   input = readFile ./sample.txt;
 in
